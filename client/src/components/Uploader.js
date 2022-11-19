4dacfe2716195e84f8b41ff271801ec834b6ecd5
Nikhil Rachawar <nikhi123rachawar@gmail.com>
2022-11-19 12:25:16 +0530

    Uploader component added

---
+++ client/src/components/Uploader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Uploader.module.css";
+
+const Uploader = () => {
+  return <div className={classes.uploader}>Uploader</div>;
+};
+
+export default Uploader;
