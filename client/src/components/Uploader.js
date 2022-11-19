178a81fdef9061bf7eabeb063156976cd827e2e7
Nikhil Rachawar <nikhi123rachawar@gmail.com>
2022-11-19 19:58:25 +0530

    On choose file functionality added

--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -1,11 +1,29 @@
-import React, { useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import classes from "./Uploader.module.css";
 
 const Uploader = () => {
   const fileInputRef = useRef();
-  const [drag, setDrag] = useState(false);
+  const [imageFile, setImageFile] = useState("");
   const [imageDropped, setImageDropped] = useState(false);
   const [imageSrc, setImageSrc] = useState("");
+
+  const imageUploadHandler = useCallback(() => {
+    const imageType = /image.*/;
+    if (imageFile) {
+      if (!imageFile.type.match(imageType)) {
+        alert("Sorry, only images are allowed");
+        return;
+      }
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        let fileURL = fileReader.result;
+        setImageSrc(fileURL);
+        setImageDropped(true);
+      };
+      fileReader.readAsDataURL(imageFile);
+    }
+  }, [imageFile]);
+
   const handleDrag = (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -19,23 +37,20 @@ const Uploader = () => {
   const handleDrop = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    setDrag(false);
-    const image = e.dataTransfer.files[0];
-    const imageType = /image.*/;
-
-    if (!image.type.match(imageType)) {
-      alert("Sorry, only images are allowed");
-      return;
-    }
-    let fileReader = new FileReader();
-    fileReader.onload = () => {
-      let fileURL = fileReader.result;
-      setImageSrc(fileURL);
-      setImageDropped(true);
-    };
-    fileReader.readAsDataURL(image);
+    setImageFile(e.dataTransfer.files[0]);
     // console.log(e.dataTransfer.files[0]);
   };
+
+  const imageInputHandler = (e) => {
+    setImageFile(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      imageUploadHandler();
+    }
+  }, [imageFile, imageUploadHandler]);
+
   return (
     <div className={classes.uploader}>
       <h1>Upload Your Image</h1>
@@ -53,6 +68,7 @@ const Uploader = () => {
               type="file"
               placeholder="Drag and drop your Image here"
               ref={fileInputRef}
+              onChange={imageInputHandler}
             />
             <div className={classes.image}>image</div>
             <label>Drag & Drop your image here</label>
