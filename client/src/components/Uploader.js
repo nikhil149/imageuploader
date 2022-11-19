7a93b092ae927c64c1fadd36bca6a58d9208d3df
Nikhil Rachawar <nikhi123rachawar@gmail.com>
2022-11-19 18:41:39 +0530

    onDrag onDrop and on dragLeave functionas added

--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./Uploader.module.css";
 
 const Uploader = () => {
-  return <div className={classes.uploader}>Uploader</div>;
+  const fileInputRef = useRef();
+  const [drag, setDrag] = useState(false);
+  const [imageDropped, setImageDropped] = useState(false);
+  const [imageSrc, setImageSrc] = useState("");
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("DRAG LEAVE");
+  };
+  const handleDragIn = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("DRAG oVER");
+  };
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDrag(false);
+    const image = e.dataTransfer.files[0];
+    const imageType = /image.*/;
+
+    if (!image.type.match(imageType)) {
+      alert("Sorry, only images are allowed");
+      return;
+    }
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      let fileURL = fileReader.result;
+      setImageSrc(fileURL);
+      setImageDropped(true);
+    };
+    fileReader.readAsDataURL(image);
+    // console.log(e.dataTransfer.files[0]);
+  };
+  return (
+    <div className={classes.uploader}>
+      <h1>Upload Your Image</h1>
+      <h6>File should be jpeg,png,..</h6>
+      <form
+        className={classes.inputDiv}
+        onDrop={handleDrop}
+        onDragOver={handleDragIn}
+        onDragLeave={handleDrag}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        {!imageDropped ? (
+          <>
+            <input
+              type="file"
+              placeholder="Drag and drop your Image here"
+              ref={fileInputRef}
+            />
+            <div className={classes.image}>image</div>
+            <label>Drag & Drop your image here</label>
+          </>
+        ) : (
+          <img src={imageSrc} alt="" />
+        )}
+      </form>
+      <span>or</span>
+      <button onClick={() => fileInputRef.current.click()}>Choose File</button>
+    </div>
+  );
 };
 
 export default Uploader;
